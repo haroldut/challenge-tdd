6ea7428ade1a85f0ff122e37cab52d265ee06bda
Harold Ulloa Torres <hautnep@gmail.com>
2021-10-08 17:30:07 -0600

    Feature #1 Add repository controller and testing

---
+++ app/Http/Controllers/RepositoryController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Repository;
+use Illuminate\Http\Request;
+
+class RepositoryController extends Controller
+{
+    public function store(Request $request) {
+        $request->validate([
+           'url' => 'required',
+            'description' => 'required',
+        ]);
+
+        $request->user()->repositories()->create($request->all());
+
+        return redirect()->route('repositories.index');
+    }
+
+    public function update(Request $request, Repository $repository) {
+        $request->validate([
+            'url' => 'required',
+            'description' => 'required',
+        ]);
+
+        if($request->user()->id != $repository->user_id)
+        {
+           abort(403);
+        }
+
+        $repository->update($request->all());
+
+        return redirect()->route('repositories.edit',$repository);
+    }
+
+    public function destroy(Request $request, Repository $repository) {
+        if($request->user()->id != $repository->user_id)
+        {
+            abort(403);
+        }
+
+        $repository->delete();
+
+        return redirect()->route('repositories.index');
+    }
+}
